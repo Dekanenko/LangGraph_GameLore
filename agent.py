3f543728bc0de129fc7840533043e28336e74fa4
Dekanenko <kirilldekanenko@gmail.com>
2024-07-21 15:02:17 +0300

    complete CRAG

--- agent.py
+++ agent.py
@@ -4,20 +4,23 @@ from langchain_core.output_parsers.string import StrOutputParser
 from langchain_core.output_parsers.pydantic import PydanticOutputParser
 from langchain.schema import Document
 from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain.retrievers.document_compressors.chain_extract import LLMChainExtractor
 from pydantic import BaseModel, Field
 from pydantic import validator
 from pathlib import Path
 from typing import Dict, TypedDict
+import numpy as np
+
 
 class GraphState(TypedDict):
     keys: Dict[str, any]
 
-
 class GraphModel():
 
-    def __init__(self, retriever, llm, threshold=0.5):
+    def __init__(self, retriever, llm, up_bound=0.8, low_bound=0.4):
         self.retriever = retriever
-        self.threshold = threshold
+        self.up_bound = up_bound
+        self.low_bound = low_bound
         self.llm = llm
 
         graph = StateGraph(GraphState)
@@ -26,6 +29,7 @@ class GraphModel():
         graph.add_node("generate", self.generate)
         graph.add_node("craft_search_query", self.craft_search_query)
         graph.add_node("web_search", self.web_search)
+        graph.add_node("refine_docs", self.refine_docs)
 
         graph.set_entry_point("retrieve")
         graph.add_edge("retrieve", "grade_docs")
@@ -33,11 +37,13 @@ class GraphModel():
             "grade_docs", 
             self.decide_to_regenerate,
             {
-                "generate": "generate",
-                "craft_search_query": "craft_search_query"
+                0: "refine_docs",
+                1: "craft_search_query",
+                2: "craft_search_query",
             })
         graph.add_edge("craft_search_query", "web_search")
-        graph.add_edge("web_search", "generate")
+        graph.add_edge("web_search", "refine_docs")
+        graph.add_edge("refine_docs", "generate")
         graph.add_edge("generate", END)
 
         self.model = graph.compile()
@@ -54,9 +60,13 @@ class GraphModel():
         question = state_dict["question"]
         documents = state_dict["documents"]
 
+        def get_page_content(documents):
+            out = "\n".join(doc.page_content for doc in documents)
+            return out
+
         prompt = PromptTemplate(input_variables=["context", "question"],  template=Path("prompts/generation.prompt").read_text())
         chain = prompt | self.llm | StrOutputParser()
-        generation = chain.invoke({"context": documents, "question": question})
+        generation = chain.invoke({"context": get_page_content(documents), "question": question})
 
         return {"keys": {"documents": documents, "question": question, "generation": generation}}
     
@@ -83,27 +93,29 @@ class GraphModel():
 
         chain = prompt | self.llm | output_parser
 
-        relevant_docs = []
-        regenerate = False
-
-        if len(documents) == 0:
-            regenerate = True
-        
+        filtered_docs = []        
+        grades = []
         for doc in documents:
-            grade = chain.invoke({"document": doc, "question": question, "format_instruction": output_parser.get_format_instructions()})
-            if grade.grade < self.threshold:
-                regenerate = True
-            else:
-                relevant_docs.append(doc)        
-
-        return {"keys": {"documents": relevant_docs, "question": question, "regenerate": regenerate}}
+            grade = chain.invoke({"document": doc.page_content, "question": question, "format_instruction": output_parser.get_format_instructions()})
+            grades.append(grade.grade)
+            if grade.grade >= self.low_bound:
+                filtered_docs.append(doc)
+        
+        grades = np.array(grades)
+        return {"keys": {"documents": filtered_docs, "question": question, "grades": grades}}
     
     def decide_to_regenerate(self, state):
-        regenerate = state["keys"]["regenerate"]
-        if regenerate:
-            return "craft_search_query"
+        grades = state["keys"]["grades"]
+
+        # 0 - correct; 1 - incorrect; 2 - ambiguous
+        strategy = 2
+
+        if (grades > self.up_bound).any():
+            strategy = 0
+        elif (grades < self.low_bound).all():
+            strategy = 1
         
-        return "generate"
+        return strategy
     
     def craft_search_query(self, state):
         state_dict = state["keys"]
@@ -129,6 +141,13 @@ class GraphModel():
         documents.append(web_docs)
 
         return {"keys": {"documents": documents, "question": question}}
-        
+    
+    def refine_docs(self, state):
+        state_dict = state["keys"]
+        question = state_dict["question"]
+        documents = state_dict["documents"]
 
+        compressor = LLMChainExtractor.from_llm(self.llm)
+        refined_docs = compressor.compress_documents(documents=documents, query=question)
 
+        return {"keys": {"question": question, "documents": refined_docs}}
--- app.py
+++ app.py
@@ -7,15 +7,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 retriever = get_vector_db().as_retriever(search_type="similarity_score_threshold", 
-                                              search_kwargs={"score_threshold": 0.05, "k": 4})
-llm = ChatOpenAI(model="gpt-4o-mini", temperature=0.2)
+                                              search_kwargs={"score_threshold": 0.05, "k": 5})
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0.1)
 graph_agent = GraphModel(retriever=retriever, llm=llm)
 
 @cl.on_chat_start
 async def main():
     await cl.Message(
-        content="Welcome to my Library, I am Adam titled 'The Greatest Librarian' here to assist you with your questions",
-        author="Adam. The Greater Librarian"
+        content="Greetings, traveler. I am Archivist Ramiel, the keeper of all knowledge within the world of Teyvat. My vast compendium spans the intricate lore, untold stories, and hidden secrets of Genshin Impact. Unlike the fragile records of Irminsul, my knowledge remains eternal and unblemished. How may I assist you on your quest for wisdom today?",
+        author="Archivist Ramiel"
     ).send()
 
 
@@ -26,5 +26,5 @@ async def main(message: str):
     response = graph_agent.model.invoke({"keys": {"question": user_input}})["keys"]["generation"]
     await cl.Message(
         content=response,
-        author="Adam. The Greater Librarian"
+        author="Archivist Ramiel"
     ).send()
